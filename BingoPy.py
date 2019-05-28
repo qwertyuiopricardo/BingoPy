d826e81fc18c004e9ec3538e0e06f474f231ed96
Ricardo Marquez <lasuerteloestodo@gmail.com>
2019-05-28 08:36:10 -0500

    carpeta obtener bolilla

--- BingoPy.py
+++
@@ -1,9 +0,0 @@
-# Pidiendo el nombre de usuario
-n = int(input("¿Cuántos jugarán?: "))
-j=[]
-for i in range (n):
-    nombre=input("Nombre de jugador: ")
-    cartilla=int(input("¿Cuántas cartillas escogerá?: "))
-    j[0][0].append(nombre)
-    j[0][1].append(cartilla)
-print(j)
