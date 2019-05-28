6019738ee5fc178f24c47846fae617b25b9648bd
Ricardo Marquez <lasuerteloestodo@gmail.com>
2019-05-28 08:01:00 -0500

    datos

--- BingoPy.py
+++ BingoPy.py
@@ -1,2 +1,9 @@
-Nombre = str(input("Ingrese su nombre: "))
-N
+# Pidiendo el nombre de usuario
+n = int(input("¿Cuántos jugarán?: "))
+j=[]
+for i in range (n):
+    nombre=input("Nombre de jugador: ")
+    cartilla=int(input("¿Cuántas cartillas escogerá?: "))
+    j[0][0].append(nombre)
+    j[0][1].append(cartilla)
+print(j)
