94c311a8b140c74708a333ddf53c0ce1c56229a8
Ricardo Marquez <lasuerteloestodo@gmail.com>
2019-05-28 08:43:55 -0500

    creado carpeta Ricardo

---
+++ Ricardo.py
@@ -0,0 +1,26 @@
+# Pidiendo el nombre de usuario
+n = int(input("¿Cuántos jugarán?: "))
+j=[]
+for i in range (n):
+    nombre=input("Nombre de jugador: ")
+    cartilla=int(input("¿Cuántas cartillas escogerá?: "))
+    m=[]
+    m.append(nombre)
+    m.append(cartilla)
+    j.append(m)
+print(j)
+
+Pozo = len(cartilla)
+
+B = []
+import random
+for i in random.randint(1,81):
+    B.append(i)
+    print(B)
+
+if B == 15:
+    Pozo = Pozo*5
+else:
+    print("Faltan números en cartilla")
+
+print(Pozo)
