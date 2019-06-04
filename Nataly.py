8eb041b89cc1012aca771a8dc0a5b906829d6119
Nataly Mogollon <natalymogollon54@gmail.com>
2019-06-04 07:38:03 -0500

    cartillas final

--- Nataly.py
+++ Nataly.py
@@ -1,19 +1,30 @@
+# Definiendo tiempo de espera después de cada bolilla
 while True:
+    import time
+    def timer(n):
+        while n!=0:
+            n=n-1
+            time.sleep(n)#time.sleep(seconds) #here you can mention seconds according to your requirement.
+            
+# Preguntar cuantos jugadores hay  
     n = int(input("¿Cuántos jugarán?: "))
     j=[]
     N=[]
-    pozo = 0
-    suma = 0
     for i in range (n):
         nombre=input("Nombre de jugador: ")
         cartilla=int(input("¿Cuántas cartillas escogerá?: "))
+        while cartilla>3:
+            cartilla = int(input("Seleccione número válido de cartillas: "))
+            if cartilla <= 3:
+                break
         N.append(cartilla)
         m=[]
         m.append(nombre)
         m.append(cartilla)
         j.append(m)
     print(j)
-
+    
+    suma=0
     for i in N:
         suma = suma + i
 
@@ -21,16 +32,37 @@ while True:
     suma2 = suma*5
     print("El pozo es: ",suma2)
     
+    import random
     B = []
     import random
     for i in range(1,81):
         B.append(i)
 
-    C = []
-    while len(C)<=15:
-        random.shuffle(B)
-        B.remove(B[0])
-        C.append(B[0])
+    while True:
+        for i in range(1):
+            C=random.choice(B)
+            B.remove(C)
 
-    print(C)
-    print("Again")
+            D=[]
+            D.append(C)
+            print(C)     
+            
+            timer(4)
+    
+            m=input("¿Deseas continuar?: ")
+        if m=="no":
+            break
+     
+    reinicio=input("¿Desea reiniciar el juego?: ")
+    if reinicio=="no":
+        break
+    print(D)
+    
+    for i in N:
+        suma = suma + i
+
+    suma2 = 0
+    suma2 = suma*5
+    print("El pozo es: ",suma2)
+    
+       
